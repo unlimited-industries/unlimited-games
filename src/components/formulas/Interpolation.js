d86f6c5ae0b4ff617b7e25ec454bc8c194ddd563
altcode <cosmosx1328@gmail.com>
2023-04-25 16:51:46 +0600

    ➕ Add katex

---
+++ src/components/formulas/Interpolation.js
@@ -0,0 +1,14 @@
+// import React from "react";
+// import { InlineMath, BlockMath } from "react-katex";
+
+// const HermitInterpolation = () => {
+//   const formula = "H_n(x) = \\sum_{i=0}^{n} f(x_i) L_{2i}^{(n)}(x) + f'(x_i) L_{2i+1}^{(n)}(x)";
+
+//   return (
+//     <div>
+//       <BlockMath>{formula}</BlockMath>
+//     </div>
+//   );
+// };
+
+// export default HermitFormula;
