d8a854454d0aa9aa84d97dae3ea84d8da82d8b54
altcode <cosmosx1328@gmail.com>
2023-04-29 23:34:24 +0600

    :sparkles: Add formulas

--- src/App.js
+++ src/App.js
@@ -1,78 +1,86 @@
-import React, { useState } from 'react';
-import './index.css';
-import TerrainGenerator from './components/TerrainGenerator';
-import {Section, Slide, LeftPane, RightPane} from './components/Section'
-import Header from './components/Header'
-import Footer from './components/Footer';
+import React, { useState } from "react";
+import "./index.css";
+import TerrainGenerator from "./components/TerrainGenerator";
+import { Section, Slide, LeftPane, RightPane } from "./components/Section";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import HermitInterpolation from "./components/formulas/Interpolation";
 
 function App() {
   const [activeSlides, setActiveSlide] = useState([0, 0]);
 
-  const slides = [[
-    {
-      leftContent: (
-        <>
-          <p className="text-white"><b>Procedural generation</b> - is a way to automatically 
-          generate game content such as terrain. Perlin noise is used to generate the terrain. 
-           <br/><br/><b>Perlin noise</b> -  is gradient noise consisting of a set of
-           pseudo-random unit vectors (gradient directions) located at specific points in space and interpolated by a smoothing function between those points.
-            To generate Perlin noise in one-dimensional space, it is necessary to calculate the value of the noise function for each point in this space, using 
-            the direction of the gradient (or slope) at the specified point.</p>
-        </>
-      ),
-      rightContent: <TerrainGenerator/>,
-    },
-    {
-      leftContent: (
-        <>
-          <p className="text-white">Lorem ipsum dolor sit amet, 
-      consectetur adipiscing elit, sed do eiusmod tempor incididunt ut 
-      labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation 
-      ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in 
-      reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur 
-      sint occaecat cupidatat non proident, sunt in culpa qui officia
-       deserunt mollit anim id est laborum.</p>
-        </>
-      ),
-      rightContent: (
-      <>
-      <p className="text-white">Lorem ipsum dolor sit amet, 
-      consectetur adipiscing elit, sed do eiusmod tempor incididunt ut 
-      labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation 
-      ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in 
-      reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur 
-      sint occaecat cupidatat non proident, sunt in culpa qui officia
-       deserunt mollit anim id est laborum.</p>
-    </>)
-    }
-  ], 
-  [
-    {
-      leftContent: (
-        <img src='fourier_transform.gif' alt=''></img>
-      ),
-      rightContent: (       
-      <>
-      <p className="text-white">Lorem ipsum dolor sit amet, 
-      consectetur adipiscing elit, sed do eiusmod tempor incididunt ut 
-      labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation 
-      ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in 
-      reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur 
-      sint occaecat cupidatat non proident, sunt in culpa qui officia
-       deserunt mollit anim id est laborum.</p>
-    </>)
-    }
-  ]];
+  const slides = [
+    [
+      {
+        leftContent: (
+          <>
+            <p className="text-white">
+              <b>Procedural generation</b> - is a way to automatically generate
+              game content such as terrain. Perlin noise is used to generate the
+              terrain.
+              <br />
+              <br />
+              <b>Perlin noise</b> - is gradient noise consisting of a set of
+              pseudo-random unit vectors (gradient directions) located at
+              specific points in space and interpolated by a smoothing function
+              between those points. To generate Perlin noise in one-dimensional
+              space, it is necessary to calculate the value of the noise
+              function for each point in this space, using the direction of the
+              gradient (or slope) at the specified point.
+            </p>
+          </>
+        ),
+        rightContent: <TerrainGenerator />,
+      },
+      {
+        leftContent: (
+          <>
+            <p className="text-white">
+              For more flexible customization of procedural generation,
+              interpolations can be used. In the mathematical field of numerical
+              analysis, interpolation is a type of estimation, a method of
+              constructing (finding) new data points based on the range of a
+              discrete set of known data points. This is used to smooth Perlin noise values.
+            </p>
+          </>
+        ),
+        rightContent: (
+          <div className="bg-black bg-opacity-60 w-5/6 h-full text-white text-opacity-80 py-16">
+            <HermitInterpolation />
+          </div>
+        ),
+      },
+    ],
+    [
+      {
+        leftContent: <img src="fourier_transform.gif" alt=""></img>,
+        rightContent: (
+          <div className="backdrop-blur-sm rounded-3xl p-3">
+            <p className="text-white text-2xl ">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+              reprehenderit in voluptate velit esse cillum dolore eu fugiat
+              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+              sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </p>
+          </div>
+        ),
+      },
+    ],
+  ];
 
   const prevSlide = (section) => {
     return () => {
       setActiveSlide((prev) => {
-        const prevIndex = (prev[0] - 1 + slides[section].length) % slides[section].length;
+        const prevIndex =
+          (prev[0] - 1 + slides[section].length) % slides[section].length;
         const newSlides = prev.slice();
         newSlides[section] = prevIndex;
         return newSlides;
       });
-    }
+    };
   };
 
   const nextSlide = (section) => {
@@ -83,7 +91,7 @@ function App() {
         newSlides[section] = nextIndex;
         return newSlides;
       });
-    }
+    };
   };
 
   return (
@@ -102,7 +110,7 @@ function App() {
         </Slide>
       </Section>
       <Section
-        bgImage="math.jpeg"
+        bgImage="formulas.png"
         activeSlide={activeSlides[1]}
         onLeftClick={prevSlide(1)}
         onRightClick={nextSlide(1)}
@@ -113,7 +121,7 @@ function App() {
           <RightPane>{slides[1][activeSlides[1]].rightContent}</RightPane>
         </Slide>
       </Section>
-      <Footer/>
+      <Footer />
     </div>
   );
 }
--- src/components/formulas/Interpolation.js
+++ src/components/formulas/Interpolation.js
@@ -1,14 +1,15 @@
-// import React from "react";
-// import { InlineMath, BlockMath } from "react-katex";
+import React from "react";
+import 'katex/dist/katex.min.css';
+import { BlockMath } from "react-katex";
 
-// const HermitInterpolation = () => {
-//   const formula = "H_n(x) = \\sum_{i=0}^{n} f(x_i) L_{2i}^{(n)}(x) + f'(x_i) L_{2i+1}^{(n)}(x)";
+const HermitInterpolation = () => {
+  const formula = "H_n(x) = \\sum_{i=0}^{n} f(x_i) L_{2i}^{(n)}(x) + f'(x_i) L_{2i+1}^{(n)}(x)";
 
-//   return (
-//     <div>
-//       <BlockMath>{formula}</BlockMath>
-//     </div>
-//   );
-// };
+  return (
+    <>
+        <BlockMath>{formula}</BlockMath>
+    </>
+  );
+};
 
-// export default HermitFormula;
+export default HermitInterpolation;
